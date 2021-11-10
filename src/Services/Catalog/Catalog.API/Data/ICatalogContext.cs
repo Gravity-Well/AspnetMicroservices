c53d401ef03f377f6e0111b67f067bf6c787f957
Nusoft <robert.tomlin@eagleteleservices.biz>
2021-11-10 17:01:22 -0500

    Create Product for CatalogDb and Interface for CatalogContext

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        // <Product> is Mongo db Product entity
+        IMongoCollection<Product> Products { get; }
+    }
+}
